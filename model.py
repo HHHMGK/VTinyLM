f90eea7e9f397e2cdff3c1d7b6c8629a316e7104
HUY <ngoduchuy71123@gmail.com>
2024-09-11 23:51:38 +0700

    fix

--- model.py
+++ model.py
@@ -1,10 +1,10 @@
-from transformers import AutoModel, AutoTokenizer
+from transformers import AutoModel, AutoTokenizer, AutoModelForCausalLM
 import torch
 import torch.nn as nn
 import copy
 
 def load_model(model_name):
-    return AutoModel.from_pretrained(model_name, trust_remote_code=True)
+    return AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
 
 def load_tokenizer(model_name):
     return AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
