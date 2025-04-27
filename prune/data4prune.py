b5079d9e14f9866c4ee2eea5b73984f9cc62e97e
HUY <ngoduchuy71123@gmail.com>
2025-04-28 01:29:22 +0700

    hot FIX !

--- prune/data4prune.py
+++ prune/data4prune.py
@@ -68,7 +68,7 @@ def get_examples(dataset, tokenizer, n_samples, seq_len = 64, rand=False, raw=Fa
                     if sample.input_ids.shape[1] >= seq_len:
                         samples.append(sample.input_ids[:, :seq_len])
                         break
-                i += 1
+            i += 1
     if raw:
         return samples
     else: 
